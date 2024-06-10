9894f86040716694d486c31b87b292fc90b6ce99
CVH4OYC <166644659+CVH4OYC@users.noreply.github.com>
2024-06-10 23:00:02 +0500

    baza

---
+++ Чат-бот для Simpl/Чат-бот для Simpl/Host.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace Чат_бот_для_Simpl
+{
+    class Host
+    {
+        TelegramBotClient _bot;
+
+        public Host(string token)
+        {
+            _bot = new TelegramBotClient(token);
+        }
+        public void Start()
+        {
+            _bot.StartReceiving(UpdateHandler, ErrorHandler);
+            Console.WriteLine("Бот запущен");
+        }
+        // это для обработки ошибок, если они будут возникать
+        private async Task ErrorHandler(ITelegramBotClient client, Exception exception, CancellationToken token)
+        {
+            Console.WriteLine($"Ошибка: {exception.Message}");
+            await Task.CompletedTask;
+        }
+        // это для приёма сообщений от бота
+        private async Task UpdateHandler(ITelegramBotClient client, Update update, CancellationToken token)
+        {
+            Console.WriteLine($"Пришло сообщение: {update.Message?.Text ?? "[не текст]"}");
+            await Task.CompletedTask;
+        }
+    }
+}
--- Чат-бот для Simpl/Чат-бот для Simpl/Program.cs
+++ Чат-бот для Simpl/Чат-бот для Simpl/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using Telegram.Bot;
+namespace Чат_бот_для_Simpl 
+{
+    class Program
+    {
+        static void Main()
+        {
+            Host bot = new Host("7043138381:AAFo4oZ1iJXH0yy9yAUCP5iKnYjgXWzOMtE");
+            bot.Start();
+            Console.ReadLine();
+        }
+    }
+}
