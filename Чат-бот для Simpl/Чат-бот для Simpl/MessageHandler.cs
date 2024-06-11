14f2b5000173e94ea6def7aa1708bdf78dbd9c20
2A42 <sizov.arteom2015@yandex.ru>
2024-06-11 17:00:19 +0500

    1. добавлен .gitignore 2. токен переселен в отдельный файл и скрыт от посторонних глаз 3. добавлена базовая обработка сообщений

---
+++ Чат-бот для Simpl/Чат-бот для Simpl/MessageHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot.Types;
+using Telegram.Bot;
+
+namespace Чат_бот_для_Simpl
+{
+    class MessageHandler
+    {
+        private long botOwnerID = 5841322604; // HINT: пока что захардкодил тут свой тг, потом можно через конфиг считывать
+
+        public async void OnMessage(ITelegramBotClient client, Update update)
+        {
+            if (update.Message?.Text == "/start")
+            {
+                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "Добро пожаловать!", replyToMessageId: update.Message?.MessageId);
+            }
+            else if (update.Message?.Text == "/help")
+            {
+                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "Мои команды\n/start\n/help", replyToMessageId: update.Message?.MessageId);
+            }
+            else
+            {
+                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, update.Message?.Text ?? "[не текст]", replyToMessageId: update.Message?.MessageId);
+            }
+        }
+    }
+}
--- Чат-бот для Simpl/Чат-бот для Simpl/obj/Debug/net8.0/Чат-бот для Simpl.AssemblyInfo.cs
+++ Чат-бот для Simpl/Чат-бот для Simpl/obj/Debug/net8.0/Чат-бот для Simpl.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Чат-бот для Simpl")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+771ba41a01504bdcc948f05472d48534d1d23ecd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+52b588f40ea3481271c674fd7c963158514f9120")]
 [assembly: System.Reflection.AssemblyProductAttribute("Чат-бот для Simpl")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Чат-бот для Simpl")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
