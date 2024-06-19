fd7e7b729c9f445c6a580ea6d369b61656e1a9a5
Vodyanik Rostislav <fillmorancheat@gmail.com>
2024-06-19 17:54:16 +0500

    Добавлена перессылка сообщений от пользователя к HR

--- Чат-бот для Simpl/Чат-бот для Simpl/MessageHandler.cs
+++ Чат-бот для Simpl/Чат-бот для Simpl/MessageHandler.cs
@@ -1,10 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Telegram.Bot.Types;
 using Telegram.Bot;
+using Telegram.Bot.Types;
 using Telegram.Bot.Types.ReplyMarkups;
 
 namespace Чат_бот_для_Simpl
@@ -12,44 +9,70 @@ namespace Чат_бот_для_Simpl
     class MessageHandler
     {
         const string ButtonFAQ = "FAQ";
-        const string ButtonHR = "Связаться с HR"; // кнопка актив
+        const string ButtonHR = "Связаться с HR";
         const string ButtonMood = "Мое настроение o(>ω<)o";
 
-        private long botOwnerID = 5841322604; // HINT: пока что захардкодил тут свой тг, потом можно через конфиг считывать
+        private long botOwnerID = 1607927336; // временное айди HR
+
+        // словарь для отслеживания состояния пользователей
+        private Dictionary<long, string> userStates = new Dictionary<long, string>();
 
         public async void OnMessage(ITelegramBotClient client, Update update)
         {
-            if (update.Message?.Text == "/start")
+            try
             {
-                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "Добро пожаловать!",replyMarkup: MyButtonsy());
+                if (update.Message?.Text == "/start")
+                {
+                    await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "Приветствую!", replyMarkup: MyButtonsy());
+                }
+                else if (update.Message?.Text == "/help")
+                {
+                    await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "Мои команды\n/start\n/help");
+                }
+                else if (update.Message?.Text == ButtonFAQ)
+                {
+                    await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "ну хз...");
+                    // do something
+                }
+                else if (update.Message?.Text == ButtonHR)
+                {
+                    await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "Пожалуйста, введите ваш вопрос.");
+                    // состояние пользователя в ожидании вопроса
+                    userStates[update.Message.Chat.Id] = "awaiting_hr_question";
+                }
+                else if (update.Message?.Text == ButtonMood)
+                {
+                    await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "(⌒‿⌒)");
+                    // do something
+                }
+                else
+                {
+                    if (userStates.ContainsKey(update.Message.Chat.Id) && userStates[update.Message.Chat.Id] == "awaiting_hr_question")
+                    {
+                        // сброс состояния пользователя
+                        userStates.Remove(update.Message.Chat.Id);
+
+                        // перессылка вопроса HR (пока себе)
+                        string userQuestion = update.Message.Text;
+                        string userName = update.Message.From.Username != null ? $"@{update.Message.From.Username}" : update.Message.From.FirstName;
+
+                        await client.SendTextMessageAsync(botOwnerID, $"Вопрос от {userName}:\n{userQuestion}");
+                        await client.SendTextMessageAsync(update.Message.Chat.Id, "Ок, HR с Вами свяжется, ожидайте.");
+                    }
+                    else
+                    {
+                        await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, update.Message?.Text ?? "[не текст]");
+                    }
+                }
             }
-            else if (update.Message?.Text == "/help")
+            catch (Exception ex)
             {
-                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "Мои команды\n/start\n/help");
-            }
-            else if (update.Message?.Text == ButtonFAQ)
-            {
-                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "ну хз...");
-                // do something
-            }
-            else if (update.Message?.Text == ButtonHR)
-            {
-                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "все ушли на обед");
-                // do something
-            }
-            else if (update.Message?.Text == ButtonMood)
-            {
-                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "(⌒‿⌒)");
-                // do something
-            }
-            else
-            {
-                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, update.Message?.Text ?? "[не текст]");
+                Console.WriteLine($"Ошибка: {ex.Message}");
+                await client.SendTextMessageAsync(update.Message?.Chat.Id ?? botOwnerID, "Произошла ошибка, попробуйте снова позже.");
             }
         }
 
         // кнопочки в клавиатуре
-        // update.Message?.Text  - название кнопки
         private IReplyMarkup MyButtonsy()
         {
             return new ReplyKeyboardMarkup(
@@ -75,7 +98,8 @@ namespace Чат_бот_для_Simpl
         // если брать такой вариант, то каждая кнопка будет посылать callbackData 
         // ибо создать inline кнопку только с текстовым полем нельзя
         //
-        // private IReplyMarkup MyButtonsy(){          
+        // private IReplyMarkup MyButtonsy()
+        // {          
         //     return new InlineKeyboardMarkup(
         //         new []{
         //             new []{
